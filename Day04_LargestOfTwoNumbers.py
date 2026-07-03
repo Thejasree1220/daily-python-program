5cbfb36477af068b43956b51f8dc61ff834f659f
Nemalla Thejasree <nemallatejasree@gmail.com>
2026-07-03 07:21:12 +0530

    Day 4: Largest of Two Numbers in Python

---
+++ Day04_LargestOfTwoNumbers.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if num1 > num2:
+    print(num1, "is the largest.")
+elif num2 > num1:
+    print(num2, "is the largest.")
+else:
+    print("Both numbers are equal.")
