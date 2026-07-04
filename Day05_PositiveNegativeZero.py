c6f3e8080a4986fe51981b22d69a4e98bd2fab05
Nemalla Thejasree <nemallatejasree@gmail.com>
2026-07-04 15:35:35 +0530

    Day 5: Positive, Negative or Zero in Python

---
+++ Day05_PositiveNegativeZero.py
@@ -0,0 +1,8 @@
+num = int(input("Enter an integer: "))
+#finding number type logic
+if num > 0:
+    print(num, "is a Positive number.")
+elif num < 0:
+    print(num, "is a Negative number.")
+else:
+    print("The number is Zero.")
