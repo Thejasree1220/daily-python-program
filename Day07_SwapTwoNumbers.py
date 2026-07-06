69e7f4b52d6a7da766db85cb5b12480e887cc38f
Nemalla Thejasree <nemallatejasree@gmail.com>
2026-07-06 10:16:56 +0530

    Day 7: Swap Two Numbers in Python

---
+++ Day07_SwapTwoNumbers.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+print("\nBefore Swapping:")
+print("First Number =", num1)
+print("Second Number =", num2)
+temp = num1
+num1 = num2
+num2 = temp
+print("\nAfter Swapping:")
+print("First Number =", num1)
+print("Second Number =", num2)
