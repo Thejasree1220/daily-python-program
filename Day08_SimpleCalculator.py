c9746d375a960b14a418d88dc65c6be00eeea280
Nemalla Thejasree <nemallatejasree@gmail.com>
2026-07-07 07:42:55 +0530

    Day 8: Simple Calculator in Python

---
+++ Day08_SimpleCalculator.py
@@ -0,0 +1,16 @@
+num1 = int(input("Enter first number: "))
+operator = input("Enter operator (+, -, *, /): ")
+num2 = int(input("Enter second number: "))
+if operator == "+":
+    print("Result =", num1 + num2)
+elif operator == "-":
+    print("Result =", num1 - num2)
+elif operator == "*":
+    print("Result =", num1 * num2)
+elif operator == "/":
+    if num2 != 0:
+        print("Result =", num1 / num2)
+    else:
+        print("Division by zero is not allowed.")
+else:
+    print("Invalid Operator")
